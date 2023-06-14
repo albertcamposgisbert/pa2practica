79a1e38547676fa6f4b36e55ef5aaa7ad821b557
Mycelgear <biasizzo.serra@gmail.com>
2023-06-14 03:22:00 +0200

    preuba github

--- class_parell_cromosomes.py
+++ class_parell_cromosomes.py
@@ -13,6 +13,7 @@ class parell_cromosomes:
     def __str__(self):
         # str method must return "parell de cromosomes" in the data type in which they are contained.
         pass
-    
+		
     def interseccio(self, parell_cromosomes):
+		print(lamamadelamamadelamamadelamamadelamamadelamama)
         pass
