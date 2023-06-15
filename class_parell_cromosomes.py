bad73307214772ba43a610ea2380e8b819405f30
Mycelgear <biasizzo.serra@gmail.com>
2023-06-15 15:27:50 +0200

    Update class_parell_cromosomes.py

--- class_parell_cromosomes.py
+++ class_parell_cromosomes.py
@@ -9,11 +9,38 @@ class parell_cromosomes:
     
     def __init__(self, cromosomes = "") -> None:
         self.__cromosomes = cromosomes
-        
+        self.__mid=len(cromosomes)/2
+        self.__cromosoma1 = cromosomes[mid:]
+        self.__cromosoma2 = cromosomes[:mid]
     def __str__(self):
         # str method must return "parell de cromosomes" in the data type in which they are contained.
-        pass
+        
+        return f"´{self.__cromosoma1}\n{self.__cromosoma2}
 		
     def interseccio(self, parell_cromosomes):
-		print(lamamadelamamadelamamadelamamadelamamadelamama)
-        pass
+        i=0
+        a1,b1=list(self.__cromosoma1[0]),list(cromosoma2[1])
+        a2,b2=list(self.__cromosoma1[0]),list(cromosoma2[1])
+        a1_a2=[]
+        b1_b2=[]
+        m=len(a1)
+        #p1=''
+        #p2=''
+        while i<m:
+            if a1[i]==a2[i]:
+                a1_a2.append(a1[i])
+            else:
+                a1_a2.append('-')
+            if b1[i]==b2[i]:
+                b1_b2.append(b1[i])
+            else:
+                b1_b2.append('-')
+            if a1_a2[i]=='-' or b1_b2[i]=='-':
+                a1_a2[i]='-'
+                b1_b2[i]='-'
+            #p1+=a1_a2[i]
+            #p2+=b1_b2[i]
+            i+=1
+        inter=(a1_a2,b1_b2)
+        #inter=(p1,p2)    
+        return inter
