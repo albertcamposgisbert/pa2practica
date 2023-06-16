39ab069d15ee6b66190d08ae063597a157db4ee2
Danlg1 <134703865+Danlg1@users.noreply.github.com>
2023-06-16 14:09:58 +0200

    changes

--- main.py
+++ main.py
@@ -17,32 +17,32 @@ while comanda != "fi":
 
     if comanda == "experiment":
         #Must generate an instance of "conjunt trets" and "conjunt d'individus"
-        conjunt_individus_instance = conjunt_individus()
-        conjunt_trets_instance= conjunt_trets()
+        conjunt_individus = conjunt_individus()
+        conjunt_trets= conjunt_trets()
 
         
     elif comanda == "consulta_individu":
         individu_id = read(int)
         print(individu_id)
-        conjunt_individus_instance.consulta_individu(individu_id)
+        conjunt_individus.consulta_individu(individu_id)
     
     elif comanda == "afegir_tret":
         #Must add tret into trets instance
         #Modificar (Incluir en trets o conjunt trets)
         tret, individu_id = read(str), read(int)
-        conjunt_trets_instance.afegir_tret(tret, individu_id) #Falta aclarar
+        conjunt_trets.afegir_tret(tret, individu_id) #Falta aclarar
         
-        pass
+        
         
     elif comanda == "treure_tret":
         #Must remove tret from trets instance
         tret, individu_id = read(str), read(int)
-        conjunt_trets_instance.treure_tret(tret, individu_id)
+        conjunt_trets.treure_tret(tret, individu_id)
 
     elif comanda == "consulta_tret":  #Si que tiene que imprimir el main porque lo que la clase le devolverá será dos cosas, la intersección y los individuos que la tienen
         
         tret = read(str)
-        info= conjunt_trets_instance.consulta_tret(tret)
+        info= conjunt_trets.consulta_tret(tret)
         if info is None:
             print("error")
         else:
