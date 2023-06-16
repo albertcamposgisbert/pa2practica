be27701a19b69e4aef2592992b802506b02429f9
Danlg1 <134703865+Danlg1@users.noreply.github.com>
2023-06-16 23:10:01 +0200

    comentarios

--- class_conjunt_trets.py
+++ class_conjunt_trets.py
@@ -1,24 +1,25 @@
 
 """
-Conjunt de Trets: L'estat (privat!) de les instàncies d'aquesta classe (en realitat
-només en caldrà una per experiment) consistirà en un diccionari de parelles
-(nom_tret, info_tret), on nom_tret és una string i info_tret pot ser una tupla
-de dos elements, una instància de Parell de Cromosomes i un conjunt d'instàncies
-d'Individu (aquells que tenen el tret). Aquí, l'expressió conjunt es refereix a
-l'estructura de dades set, que Python ens proporciona
-. Quines operacions us fan falta (els mètodes públics) és quelcom que heu de decidir vosaltres.
+Conjunto de características: el estado (¡privado!) de las instancias de esta clase (en realidad
+sólo será necesaria una por experimento) consistirá en un diccionario de pares clave valor donde la clave será el
+el nombre de la características indicadas e info_tret será una tupla
+de dos elementos: una instancia de Par de Cromosomas y una lista de instancias
+de Individuo (aquellos que tienen la característica)
 """
 from class_conjunt_individus import *
 from class_parell_cromosomes import *
 from collections import namedtuple
 from bintree import BinTree
 
-Element = namedtuple("Element", ["interseccio", "individus"])
-
+Element = namedtuple("Element", ["interseccio", "individus"]) 
 
 class conjunt_trets:
     
     def __init__(self,conj):
+        """
+        Esta clase se instancia en el main usando de argumento la instancia de la anteriormente
+        inicializada clase conjunt_individus y se crea el diccionario que usaremos.
+        """
         self.__conjunt_individus=conj
         self.__trets={}
 
@@ -55,84 +56,90 @@ class conjunt_trets:
 
     def afegir_tret(self,nom_tret,numero_individu):
         """
-        Assigna informació a una clau. Si la clau ja hi és dins
-        el diccionari, la informació és modificada.
-        cas pitjor: Theta(n). cas mitjà: Theta(1+n/M).
+        Asigna información a una clave. Si la clave ya esta en el diccionario hacemos los cambios correspondientes
+        (añadimos el nuevo individuo(añadimos su instancia de clase individuo) y la nueva intersección). Si el 
+        individuo dado ya se había introducido se imprime un error y no se añade. Lo mismo sucede si el individuo
+        no es uno de los indicados inicialmente
         """
-        m=self.__conjunt_individus.get_num_individuos()
+        m=self.__conjunt_individus.get_num_individuos() #Obtenemos el número de individuos, que también es el máximo
+                                                        #individuo que podemos tener y todo individuo de número más grande
+                                                        #no es uno de los indicados al inicio(tampoco si es menor que 1)
         if numero_individu>m or numero_individu<1:
             print('error')
         else:
-            if nom_tret in self.__trets: #Això vol dir que el tret ja el teniem
-                elem=self.__trets[nom_tret]
+            if nom_tret in self.__trets: #Si tenemos ya la característica en el diccionario vemos primero
+                elem=self.__trets[nom_tret] #si ya hemos introducido ese individuo, en ese caso imprimimos un error
                 for i in elem.individus:
                     if i.get_id_by_individu() == numero_individu:
                         print('error')
-                        return 0
-                self.__conjunt_individus.afegir_tret(nom_tret,numero_individu)
-           
-                interseccio_original=elem.interseccio
-
+                        return 0                                                
+                self.__conjunt_individus.afegir_tret(nom_tret,numero_individu)#Si llamamos a el método hómologo a este de la clase conjunt_individus
+                                                                              #y añadimos al individuo correspondiente la característica indicada                                            
+                interseccio_original=elem.interseccio                         #a continuación introducimos la instancia correspondiente de la clase individuos
+                                                                              #en nuestra lista y encontramos la nueva intersección, que también guardamos en el diccionario
                 cromosomes_nou_element=self.__conjunt_individus.get_individu_by_id(numero_individu).get_parell_cromosomes()
                 nova_interseccio=interseccio_original.interseccio(cromosomes_nou_element)
                 elem.individus.append(self.__conjunt_individus.get_individu_by_id(numero_individu))
-
                 self.__trets[nom_tret]=Element(nova_interseccio,elem.individus)
-            else:
-
-                self.__conjunt_individus.afegir_tret(nom_tret,numero_individu)
-
+            else: #Si no estaba en el diccionario ya, cogemos su par de cromosomas y lo guardamos como intersección
+                  #ademas de guardar en una lista, que también creamos ahora, el individuo que tendrá esta característica
+                  #la tupla resultante será el valor del diccionario y la clave el nombre de la característica.
+                self.__conjunt_individus.afegir_tret(nom_tret,numero_individu) #como antés, añadimos la característica a la lista de características del individuo en la clase conjunt_individuos
                 cromosomas=self.__conjunt_individus.get_individu_by_id(numero_individu).get_parell_cromosomes()
-                
                 self.__trets[nom_tret]=Element(cromosomas,[self.__conjunt_individus.get_individu_by_id(numero_individu)])
     def treure_tret(self, tret, numero_individu):
         """
-        Elimina la parella (clau, valor) del diccionari. Si la clau no pertany al
-        diccionari, res canvia.
-        cas pitjor: Theta(n). cas mitjà: Theta(1+n/M).
+        Elimina de la lista de individuos de la característica correspondiente el individuo indicado y se
+        recalcula la intersección si es necesario o se elimina la característica.
         """
         
-        if tret in (self.__trets):
+        if tret in (self.__trets): #miramos si la característica en cuestión está en el diccionario
             tetret=False
             i=0
             element=self.__trets[tret]
             individuos= element.individus
 
-            for a in individuos:
+            for a in individuos: #si el individuo no está para ser eliminado imprimimos error
                 if a.get_id_by_individu() != numero_individu:
                     print('error')
                     break
 
-            while i < len(individuos):
+            while i < len(individuos): #Encontramos el individuo indicado y lo eliminamos
                 if individuos[i].get_id_by_individu() == numero_individu:
                     tetret=True
                     individuos.pop(i)
                     break
                 i+=1
 
-            if len(individuos)<1 : 
-                self.__conjunt_individus.treure_tret(tret,numero_individu)
+            if len(individuos)<1 : #si el tamaño de la lista de individuos resultante es menor que uno eliminamos
+                                    #la característica
+                self.__conjunt_individus.treure_tret(tret,numero_individu)#llamamos al homologo de mismo nombre de la clase conjunt_individus para eliminar la característica
+                                                                          #de la lista de características del individuo
                 del self.__trets[tret]
                 tetret=False
             
-            elif tetret:
+            elif tetret: #en caso contrario recalculamos la intersección y guardamos la nueva intersección y 
+                         #la nueva lista de individuos 
+                        
                 intersection=individuos[0].get_parell_cromosomes()
-                self.__conjunt_individus.treure_tret(tret,numero_individu)
+                self.__conjunt_individus.treure_tret(tret,numero_individu)#llamamos al homologo de mismo nombre de la clase conjunt_individus para eliminar la característica
+                                                                          #de la lista de características del individuo
 
                 for i in individuos:
                     cromosomas=i.get_parell_cromosomes()
                     intersection= intersection.interseccio(cromosomas)
                 self.__trets[tret]=Element(intersection,individuos)
-        else:
+        else: #si no existe el tret del que queremos eliminar un individuo mostramos error
             print('error')
         
                 
     def consulta_tret(self, tret):
         """
-        retorna el valor associat a una clau, None si la clau no hi és
-        cas pitjor: Theta(n). cas mitjà: Theta(1+n/M).
+        Retorna una tupla de strings del par de cromosomas de la intersección de la característica indicada
+        y la lista de individuos que poseen la característica.
         """
-        if tret in self.__trets:
+        if tret in self.__trets: #solo si existe el tret retornamos los valores, en caso contrario retornamos un None
+                                 #que hará que el main retorne error.
             element=self.__trets[tret]
             interseccio = element.interseccio.get_cromosomas()
             individus=element.individus
--- class_parell_cromosomes.py
+++ class_parell_cromosomes.py
@@ -19,30 +19,34 @@ class parell_cromosomes:
             
             return (self.__cromosoma1,self.__cromosoma2)	
     def interseccio(self,e1):
+        """
+        Encuentra la intersección entre dos instancias de parell_cromosomes y la devuelve como una nueva instancia
+        de esta clase.
+        """
         i=0
-        a1,b1=list(self.__cromosoma1),list(self.__cromosoma2)
-        a2,b2=list(e1.__cromosoma1),list(e1.__cromosoma2)
+        a1,b1=list(self.__cromosoma1),list(self.__cromosoma2) #guardamos en forma de lista el primer y segundo cromosoma de la instancia self
+        a2,b2=list(e1.__cromosoma1),list(e1.__cromosoma2)     #hacemos lo mismo con la instancia dada como argumento
 
         a1_a2=[]
         b1_b2=[]
         m=len(a1)
         p1=''
         p2=''
-        while i<m:
-            if a1[i]==a2[i]:
+        while i<m: #mientras no hayamosrecorrido todos digitos de los cromosomas miramos la intersección
+            if a1[i]==a2[i]: #si son iguales lo guardamos(intersección)
                 a1_a2.append(a1[i])
-            else:
+            else: #si no ponemos un guión
                 a1_a2.append('-')
-            if b1[i]==b2[i]:
+            if b1[i]==b2[i]: #lo mismo pero con los segundos cromosomas de cada par
                 b1_b2.append(b1[i])
             else:
                 b1_b2.append('-')
-            if a1_a2[i]=='-' or b1_b2[i]=='-':
+            if a1_a2[i]=='-' or b1_b2[i]=='-': #si alguna de las dos elementos guardados es un guión, forzamos el guión en los dos 
                 a1_a2[i]='-'
                 b1_b2[i]='-'
-            p1+=a1_a2[i]
+            p1+=a1_a2[i] #guardamos las intersecciones encontradas como string
             p2+=b1_b2[i]
             i+=1
-        a=p1+p2
+        a=p1+p2 #creamos una instancia de parell_cromosomes y eso retornamos.
         cl= parell_cromosomes(a)
         return cl
